2dfd667c7a43fab90664b551d83e5e56ccb62a9f
sharf <373455742@qq.com>
2016-10-09 17:39:02 +0800

    冲值报表

--- webservices/report_recharge.php
+++ webservices/report_recharge.php
@@ -23,30 +23,30 @@ if(empty($_GET['ui']))
 	
 	<?
 	{		
-		$sql="SELECT SUM( money ) money, SUM( ABS( money_feiyong ) ) feiyong, LEFT( riqi, 7 ) riqi FROM ecm_my_moneylog WHERE leixing =100 GROUP BY LEFT( riqi, 7 ) ";
-		$result=$db->get_all($sql);	
-		?>	
+		$sql="SELECT SUM( money ) money, LEFT( riqi, 7 ) riqi FROM {moneylog} WHERE `type` =100 GROUP BY LEFT( riqi, 7 ) ";
+		$result=$db->get_all($sql);
+		?>
 		<table cellpadding="4" cellspacing="1" width="100%" bgcolor="#CCCCCC">
 		<form method="GET" id='form1' name='form1'>
-        
-		<?	
-		echoTh(array('日期','充值金额(元)','到帐金额(元)','费用(元)'));	
+
+		<?
+		echoTh(array('日期','充值金额(元)','到帐金额(元)','费用(元)'));
 		$money_sum=0;
 		$feiyong_sum=0;
 
 		foreach ($result as $row)
 		{
 			$money_sum+=$row['money'];
-			$feiyong_sum+=$row['feiyong'];
+			$feiyong_sum+=(float)$row['feiyong'];
 			$recharge=$row['money']+$row['feiyong'];
 			$recharge_sum+=$recharge;
 			?>
 			<tr <?=getChangeTr()?>>
             	<td align='left'>&nbsp;&nbsp;<?=$row['riqi']?></td>
                 <td align='left'>&nbsp;&nbsp;<?=$recharge?></td>
-                <td align='left'>&nbsp;&nbsp;<?=$row['money']?></td>            	
-                <td align='left'><?=$row['feiyong']?></td>  
-               
+                <td align='left'>&nbsp;&nbsp;<?=$row['money']?></td>
+                <td align='left'><?=$row['feiyong']?></td>
+
 			</tr>
 			<?
 		}
@@ -54,38 +54,38 @@ if(empty($_GET['ui']))
         <tr <?=getChangeTr()?>>
             <td align='left'>&nbsp;&nbsp;总计</td>
             <td align='left'>&nbsp;&nbsp;<?=$recharge_sum?></td>
-            <td align='left'>&nbsp;&nbsp;<?=$money_sum?></td>            	
-            <td align='left'><?=$feiyong_sum?></td>  
-           
+            <td align='left'>&nbsp;&nbsp;<?=$money_sum?></td>
+            <td align='left'><?=$feiyong_sum?></td>
+
         </tr>
-        </form></table>	
+        </form></table>
         <br /><br />
         <h2>充值小于200</h2>
-       <? 
-        $sql="SELECT SUM( money ) money, SUM( ABS( money_feiyong ) ) feiyong, LEFT( riqi, 7 ) riqi FROM ecm_my_moneylog WHERE leixing =100 and (money+abs(money_feiyong))<200 GROUP BY LEFT( riqi, 7 ) ";
-		$result=$db->get_all($sql);	
-		?>	
+       <?
+        $sql="SELECT SUM( money ) money, LEFT( riqi, 7 ) riqi FROM {moneylog} WHERE `type` =100 and money<200 GROUP BY LEFT( riqi, 7 ) ";
+		$result=$db->get_all($sql);
+		?>
 		<table cellpadding="4" cellspacing="1" width="100%" bgcolor="#CCCCCC">
 		<form method="GET" id='form1' name='form1'>
-        
-		<?	
-		echoTh(array('日期','充值金额(元)','到帐金额(元)','费用(元)'));	
+
+		<?
+		echoTh(array('日期','充值金额(元)','到帐金额(元)','费用(元)'));
 		$money_sum=0;
 		$feiyong_sum=0;
 $recharge_sum=0;
 		foreach ($result as $row)
 		{
 			$money_sum+=$row['money'];
-			$feiyong_sum+=$row['feiyong'];
+			$feiyong_sum+=(float)$row['feiyong'];
 			$recharge=$row['money']+$row['feiyong'];
 			$recharge_sum+=$recharge;
 			?>
 			<tr <?=getChangeTr()?>>
             	<td align='left'>&nbsp;&nbsp;<?=$row['riqi']?></td>
                 <td align='left'>&nbsp;&nbsp;<?=$recharge?></td>
-                <td align='left'>&nbsp;&nbsp;<?=$row['money']?></td>            	
-                <td align='left'><?=$row['feiyong']?></td>  
-               
+                <td align='left'>&nbsp;&nbsp;<?=$row['money']?></td>
+                <td align='left'><?=$row['feiyong']?></td>
+
 			</tr>
 			<?
 		}
@@ -93,14 +93,14 @@ $recharge_sum=0;
         <tr <?=getChangeTr()?>>
             <td align='left'>&nbsp;&nbsp;总计</td>
             <td align='left'>&nbsp;&nbsp;<?=$recharge_sum?></td>
-            <td align='left'>&nbsp;&nbsp;<?=$money_sum?></td>            	
-            <td align='left'><?=$feiyong_sum?></td>  
-           
+            <td align='left'>&nbsp;&nbsp;<?=$money_sum?></td>
+            <td align='left'><?=$feiyong_sum?></td>
+
         </tr>
-        </form></table>	
+        </form></table>
         <br /><br />
          <h2>充值大于等于200</h2>
-       <? $sql="SELECT SUM( money ) money, SUM( ABS( money_feiyong ) ) feiyong, LEFT( riqi, 7 ) riqi FROM ecm_my_moneylog WHERE leixing =100 and (money+abs(money_feiyong))>=200 GROUP BY LEFT( riqi, 7 ) ";
+       <? $sql="SELECT SUM( money ) money, LEFT( riqi, 7 ) riqi FROM {moneylog} WHERE `type` =100 and money>=200 GROUP BY LEFT( riqi, 7 ) ";
 		$result=$db->get_all($sql);	
 		?>	
 		<table cellpadding="4" cellspacing="1" width="100%" bgcolor="#CCCCCC">
@@ -114,7 +114,7 @@ $recharge_sum=0;
 		foreach ($result as $row)
 		{
 			$money_sum+=$row['money'];
-			$feiyong_sum+=$row['feiyong'];
+			$feiyong_sum+=(float)$row['feiyong'];
 			$recharge=$row['money']+$row['feiyong'];
 			$recharge_sum+=$recharge;
 			?>
